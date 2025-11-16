7274de03746fc9b1e9743378f00b2f88eac5f975
Tanjim Noor <tanjim.noor@g.bracu.ac.bd>
2025-11-17 05:20:35 +0600

    fix:

--- frontend/src/components/admin/QuestionBank/QuestionFormDialog.tsx
+++ frontend/src/components/admin/QuestionBank/QuestionFormDialog.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Box, MenuItem, CircularProgress } from '@mui/material'
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Box, MenuItem, CircularProgress, Autocomplete, Chip } from '@mui/material'
 import { useForm, Controller } from 'react-hook-form'
 import type { Question, QuestionFormData, QuestionType } from '@/types/question.types'
 import { createQuestion, updateQuestion } from '@/api/questions'
@@ -115,6 +115,25 @@ const QuestionFormDialog: React.FC<Props> = ({ open, onClose, question, onSucces
                       )}
                     </>
                   )}
+
+          <Controller name="tags" control={control as any} render={({ field }) => (
+            <Autocomplete
+              multiple
+              freeSolo
+              options={[]}
+              value={field.value ?? []}
+              onChange={(_, newValue) => field.onChange(newValue)}
+              renderInput={(params) => (
+                <TextField {...params} label="Tags" placeholder="Add tags (press Enter)" helperText="Type and press Enter to add tags" />
+              )}
+              renderTags={(value, getTagProps) =>
+                value.map((option, index) => {
+                  const { key, ...tagProps } = getTagProps({ index })
+                  return <Chip key={key} label={option} size="small" {...tagProps} />
+                })
+              }
+            />
+          )} />
 
           <Controller name="max_score" control={control as any} rules={{ required: true, min: 1 }} render={({ field }) => (
             <TextField label="Max Score" type="number" inputProps={{ min: 1 }} {...field} />
